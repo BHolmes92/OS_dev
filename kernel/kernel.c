2f4ae6286f25bf36ff046a4dacadedb5e2d3cb7c
Ben <b3holmes@uwaterloo.ca>
2023-08-29 15:39:23 -0400

    Basic test cases for text printing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,9 +2,7 @@
 
 void main(){
     clear_screen();
-    //print_at("X", 1, 6);
-    //print_at("Multiline Spanning Text", 75, 10);
-    //print_at("This has a line break\nbreak in it", 0, 20);
-    //print("Another line break");
-    //print_at("Testing the scrolling functions", 45, 24);
+    print_at("X", 1, 6);
+    print_at("This text should span multiple lines", 75, 10);
+    print_at("This should test running off the bottom of screen", 45, 24);
 }
