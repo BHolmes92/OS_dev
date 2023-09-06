76f5c7fdf504fcb1ba1ccf8e3b86b46b90b59de0
Ben <b3holmes@uwaterloo.ca>
2023-09-06 17:38:06 -0400

    Added interupt Code

--- cpu/isr.c
+++ cpu/isr.c
@@ -2,8 +2,12 @@
 #include "isr.h"
 #include "../drivers/screen.h"
 #include "../kernel/util.h"
+#include "../drivers/low_level.h"
+
+isr_t interrupt_handlers[256];
 
 void isr_install(){
+    //Set up Interupt Descriptor Table 0-31
     set_idt_gate(0, (uint32_t)isr0);
     set_idt_gate(1, (uint32_t)isr1);
     set_idt_gate(2, (uint32_t)isr2);
@@ -36,6 +40,36 @@ void isr_install(){
     set_idt_gate(29, (uint32_t)isr29);
     set_idt_gate(30, (uint32_t)isr30);
     set_idt_gate(31, (uint32_t)isr31);
+
+    //Remap the PIC to after IDT vector
+    port_byte_out(PIC_MASTER_CMD, 0x11); //Initialize Command, expects 3 parameters 
+    port_byte_out(PIC_SLAVE_CMD, 0x11);
+    port_byte_out(PIC_MASTER_DATA, 0x20);//Master PIC Vector offset
+    port_byte_out(PIC_SLAVE_DATA, 0x28); //Slave PIC Vector offset
+    port_byte_out(PIC_MASTER_DATA, 0x04);//Configuration: Slave Present
+    port_byte_out(PIC_SLAVE_DATA, 0x02); //Cascade Configuration
+    port_byte_out(PIC_MASTER_DATA, 0x01); //Environment ICW4 Present
+    port_byte_out(PIC_SLAVE_DATA, 0x01);  //Environment ICW4 Present
+    port_byte_out(PIC_MASTER_DATA, 0x00); //8080 Mode
+    port_byte_out(PIC_SLAVE_DATA, 0x00);  //8080 Mode
+
+    //Set up IRQ handlers 32-47
+    set_idt_gate(IRQ0, (uint32_t)irq0);
+    set_idt_gate(IRQ1, (uint32_t)irq1);
+    set_idt_gate(IRQ2, (uint32_t)irq2);
+    set_idt_gate(IRQ3, (uint32_t)irq3);
+    set_idt_gate(IRQ4, (uint32_t)irq4);
+    set_idt_gate(IRQ5, (uint32_t)irq5);
+    set_idt_gate(IRQ6, (uint32_t)irq6);
+    set_idt_gate(IRQ7, (uint32_t)irq7);
+    set_idt_gate(IRQ8, (uint32_t)irq8);
+    set_idt_gate(IRQ9, (uint32_t)irq9);
+    set_idt_gate(IRQ10, (uint32_t)irq10);
+    set_idt_gate(IRQ11, (uint32_t)irq11);
+    set_idt_gate(IRQ12, (uint32_t)irq12);
+    set_idt_gate(IRQ13, (uint32_t)irq13);
+    set_idt_gate(IRQ14, (uint32_t)irq14);
+    set_idt_gate(IRQ15, (uint32_t)irq15);
 
     set_idt();
 }
@@ -87,4 +121,20 @@ void isr_handler(registers_t r){
     print("\n");
     print(exception_messages[r.int_no]);
     print("\n");
+}
+
+void irq_handler(registers_t r){
+    if(r.int_no >= 40){
+        port_byte_out(PIC_SLAVE_CMD, 0x20); //Reset the slave PIC if used
+    }
+    port_byte_out(PIC_MASTER_CMD, 0x20); //Reset master PIC
+
+    if(interrupt_handlers[r.int_no] != 0){
+        isr_t handler = interrupt_handlers[r.int_no];
+        handler(r);
+    }
+}
+
+void register_interupt_handler(uint8_t n, isr_t handler){
+    interrupt_handlers[n] = handler;
 }
--- cpu/isr.h
+++ cpu/isr.h
@@ -3,6 +3,30 @@
 
 #include <stdint.h>
 
+//Define CMD values
+#define PIC_MASTER_CMD 0x0020
+#define PIC_MASTER_DATA 0x0021
+#define PIC_SLAVE_CMD 0x00A0
+#define PIC_SLAVE_DATA 0x00A1
+
+//Remapped IRQ postions
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
 //Intel Reserved ISRs 0-31
 extern void isr0();
 extern void isr1();
@@ -37,6 +61,24 @@ extern void isr29();
 extern void isr30();
 extern void isr31();
 
+//IRQs 32-47
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
 typedef struct{
     uint32_t ds;
     uint32_t edi, esi, ebp, esp, ebx, ecx, eax;  //Registers to be pushed
@@ -46,5 +88,10 @@ typedef struct{
 
 void isr_install();
 void isr_handler(registers_t r);
+
+void irq_handler(registers_t r);
+typedef void (*isr_t)(registers_t);
+
+void register_interupt_handler(uint8_t n, isr_t handler);
 
 #endif
