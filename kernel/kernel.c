eddb2286584fcae944b05ede24668aad97e9a26f
Ben <b3holmes@uwaterloo.ca>
2023-09-06 14:33:00 -0400

    Call ISR setup function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,8 @@
 #include "../drivers/screen.h"
+#include "../cpu/isr.h"
 
 void main(){
+    isr_install();
     print("Testing Interupts");
     __asm__ __volatile__("int $2");
     __asm__ __volatile__("int $3");
