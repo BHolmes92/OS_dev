478bf1c8b336d4b3f2c0184554fed8d5c73f9640
Ben <b3holmes@uwaterloo.ca>
2023-09-14 00:25:42 -0400

    While loop to for loop

--- kernel/util.c
+++ kernel/util.c
@@ -11,11 +11,12 @@ void memory_copy(char* source, char* dest, int no_bytes){
 //Set the destination to value
 void memory_set(uint8_t* dest, uint8_t val, uint32_t len){
     uint8_t* temp = (uint8_t*)dest; //Ensure expected pointer size
-    while(len > 0){
+    /* while(len > 0){
         *temp = (uint8_t) val;
         temp++;
         len--;
-    }
+    } */
+    for(; len != 0 ; len--) *temp++ = val;
 }
 
 void int_to_ascii(int n, char str[]){
