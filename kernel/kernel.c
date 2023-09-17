a8763f8d15f2fc9472f695508abc4cb5cac5a02b
Ben <b3holmes@uwaterloo.ca>
2023-09-16 22:22:51 -0400

    Added keyboard isr

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,11 @@
 #include "../drivers/screen.h"
 #include "../cpu/isr.h"
 #include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main(){
     isr_install();
     __asm__ __volatile__("sti");
-    init_timer(50);
+    //init_timer(50);
+    init_keyboard();
 }
